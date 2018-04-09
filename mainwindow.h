142bbb0125ef94bc62ee02acdb9cff4402bf1275
aoifepower123 <15140679@studentmail.ul.ie>
2018-04-09 14:36:33 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,50 +1,76 @@
-#ifndef ROOM_H_
-#define ROOM_H_
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
-#include <QString>
-#include <QPixmap>
-#include <string>
-#include <map>
-#include <vector>
+#include <QMainWindow>
+#include "ZorkUL.h"
 #include "item.h"
-#include "notes.h"
-#include "keys.h"
+namespace Ui {
+class MainWindow;
+}
 
-using namespace std;
-using std::vector;
-
-class Room {
-
-private:
-    QString description;
-    QString viewPath;
-    QString keyID;
-    vector<Room*> exits;
-    QString exitString();
-    vector<Item*> itemsInRoom;
-    QPixmap roomInMapImage;
-    bool upstairs;
-    bool downstairs;
-    bool locked;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
 
 public:
-    Room(QString = "", QString = "", QString = "", bool = false, bool = false, bool = false, QString = "");
-    void setExits(vector<Room*>&);
-    void addNote(notes*);
-    void addKeys(keys*);
-    vector<Room*> getExits();
-    vector<Item*> getItems();
-    QString shortDescription();
-    QString longDescription();
-    bool canGoUp();
-    bool canGoDown();
-    bool checkIfLocked();
-    QPixmap getPixmap();
-    QString getViewPixmap();
-    QString getNotepath();
-    void clearRoomOfItems();
-    bool checkUnlockDoor(keys*);
-    void setLocked(bool);
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    //void on_teleButton_clicked();
+
+    //void on_startButton_clicked();
+
+   // void on_mapButton_clicked();
+
+    //void on_testButton_clicked();
+
+    void on_goButton_clicked();
+
+    void on_upButton_clicked();
+
+    void on_downButton_clicked();
+
+    void on_SearchButton_clicked();
+
+    void on_UseItem_clicked();
+
+    void on_item1_clicked();
+
+    void on_item2_clicked();
+
+    void on_item3_clicked();
+
+    void on_item4_clicked();
+
+    void on_item5_clicked();
+
+    void on_item6_clicked();
+
+    void on_item7_clicked();
+
+    void on_item8_clicked();
+
+    void on_item9_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    ZorkUL *zork;
+    int inventoryItemCounter;
+    vector<Item*> inventoryItems;
+
+    void check_exit_rooms();
+    void displayCurrentRoomInfo();
+    void displayExitList();
+    void clearExitList();
+    void floorMoveAllowed();
+    void on_unlocked_Door(QString, int);
+
+    void inventoryDisplay();
+    void displayItemContents();
+    void addItemToInventoryGUI(Item*);
+    void insertToGUI(notes*);
+    void insertToGUI(keys*);
 };
 
-#endif
+#endif // MAINWINDOW_H
